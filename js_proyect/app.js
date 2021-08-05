0f1f6c6198fefa35a6fecda9695a8245c3693285
onialuis88 <81720811+onialuis88@users.noreply.github.com>
2021-08-04 23:55:49 -0300

    Add files via upload

---
+++ js_proyect/app.js
@@ -0,0 +1,20 @@
+let bienvenida = prompt("Hola, me indicaría su nombre por favor?");
+
+alert("Bienvenido " + bienvenida);
+alert("La pagina esta en construcción pero, aguarda a proximas actualizaciones :) ");
+
+let edad = parseInt(prompt("Ingrese su edad: "));
+anios(edad);
+
+function anios(edad){
+    if (edad > 18){
+        alert("Tiene la edad para comprar cualquier artículo");
+    }
+    else if (edad < 18){
+        alert("Tiene prohibido comprar bebidad alcohólicas");
+    }
+}
+
+document.getElementById("nombre").value = bienvenida;
+document.getElementById("edad").value   = edad;
+
