040c933ae5c118e8b3a0ad8109f47795bf98a8c3
onialuis88 <81720811+onialuis88@users.noreply.github.com>
2021-08-11 22:29:41 -0300

    Add files via upload

---
+++ TP2/app.js
@@ -0,0 +1,7 @@
+var enviarinformacion = document.querySelector("#enviarinformacion");
+
+enviarinformacion.addEventListener("click", alerta);
+
+function alerta(){
+    alert("Se envio su mensaje");
+}
