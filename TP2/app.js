e4aaa4b4b4a82b7fda23ba3d9937f16bd251275c
onialuis88 <81720811+onialuis88@users.noreply.github.com>
2021-09-19 21:40:09 -0300

    Delete TP2 directory

--- TP2/app.js
+++
@@ -1,7 +0,0 @@
-var enviarinformacion = document.querySelector("#enviarinformacion");
-
-enviarinformacion.addEventListener("click", alerta);
-
-function alerta(){
-    alert("Se envio su mensaje");
-}
