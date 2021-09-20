0d8caa196fbd602f3de3cc5b0afc7bfe645315e5
onialuis88 <81720811+onialuis88@users.noreply.github.com>
2021-09-19 21:40:20 -0300

    Delete constructor.js

--- constructor.js
+++
@@ -1,7 +0,0 @@
-export class producto {
-    constructor(instrumento, precio, telefono) {
-        this.instrumento = instrumento;
-        this.precio = precio;
-        this.telefono = telefono;
-    }
-}
